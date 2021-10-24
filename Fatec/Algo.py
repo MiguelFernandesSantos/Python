fa40f97726f9c3049eae99bd61b5db3f879eb054
MiguelFernandesSantos <89362843+MiguelFernandesSantos@users.noreply.github.com>
2021-10-24 11:12:53 -0300

    Update Algo.py

--- Fatec/Algo.py
+++ Fatec/Algo.py
@@ -1,44 +1,205 @@
-# Dicionario com 3 listas e 1 string:
-Dados = {
-        
-        "Numeros" : [],
-        "Pesos" : [],
-        "Atuais" : [None for _ in range(2)],
-        "Menu" : ""
+######################################################
+#        TRABALHO FINAL - OFICINA DE PYTHON          #
+#                                                    #
+#    NOMES: DAVID GALVÃO MANDU DA SILVA;             #
+#           MIGUEL FERNANDES SANTOS.                 #
+#                                                    #
+#    DATA: --/--/2021                                #
+#    PROJETO: CÁLCULOS DE ESTATÍSTCA BÁSICA.         #
+#                                                    #
+######################################################
 
-        }
+import math
 
-# Enquanto o usuario nao digitar SAIR ou 2:
-while (Dados["Menu"] != "Sair" or Dados["Menu"] != "2"):
+# ##############PROCEDIMENTOS##############
+
+#             1 - MÉDIA ARITMÉTICA SIMPLES
+def mediasimples(valores=()):
+    valores.sort()
+    print(f'Amostra ordenada = {valores}')
+    media = 0
+
+    for item in valores:
+        media += item
+
+    media = media / qtde_elementos
+    print(f'Média Aritmética Simples = {media} ')
+    return media
+#####################################################
+
+
+#             2 - MÉDIA ARITMÉTICA PONDERADA
+def mediaponderada(valores=()):
+    valores.sort()
+    print(f'Amostra ordenada = {valores}')
+    valorxpesos = 0
+    soma_peso = 0
+
+    for item in valores:
+        peso = int(input(f'Digite o peso do valor {item}: '))
+        valorxpesos += (peso * item)
+        soma_peso += peso
+
+    media_ponderada = valorxpesos / soma_peso
+    print(f'Média Aritmética Ponderada = {media_ponderada}')
+#####################################################
+
+
+#             3 - MODA
+def moda(valores=()):
+    valores.sort()
+    print(f'Amostra ordenada = {valores}')
+    dic = {item: 0 for item in valores}
+    for item in valores:
+        if item in dic.keys():
+            dic[item] += 1
+    valor_moda = [item for item in dic.values()]
+    resultado_moda = [numero for numero in dic.keys() if dic[numero] == max(valor_moda) and max(valor_moda) > 1]
+
+    if len(resultado_moda) != 0:
+        print(f' Moda = {resultado_moda}')
+    else:
+        print('Amostra Amodal')
+#####################################################
+
+
+#             4 - MEDIANA
+def mediana(valores=()):
+    valores.sort()
+    print(f'Amostra ordenada = {valores}')
+    if qtde_elementos % 2 == 0:
+        med = (valores[int((qtde_elementos / 2) - 1)] + valores[int((qtde_elementos / 2))]) / 2
+        print(f'Mediana = {med}')
+    else:
+        med = valores[int((qtde_elementos / 2))]
+        print(f'Mediana = {med}')
+#####################################################
+
+
+#             5 - PRIMEIRO QUARTIL
+def primeiroquartil(valores=()):
+    valores.sort()
+    print(f'Amostra ordenada = {valores}')
+    if qtde_elementos % 4 == 0:
+        q1 = (valores[int((qtde_elementos / 4) - 1)] + valores[int((qtde_elementos / 4))]) / 2
+        print(f'Primeiro Quartil = {q1}')
+    else:
+        q1 = valores[int((qtde_elementos / 4))]
+        print(f'Primeiro Quartil = {q1}')
+#####################################################
+
+
+#             6 - TERCEIRO QUARTIL
+def terceiroquartil(valores=()):
+    valores.sort()
+    print(f'Amostra ordenada = {valores}')
+    if (3 * qtde_elementos) % 4 == 0:
+        q3 = (valores[int((3 * qtde_elementos / 4) - 1)] + valores[int((3 * qtde_elementos / 4))]) / 2
+        print(f'Terceiro Quartil = {q3}')
+    else:
+        q3 = valores[int(((3 * qtde_elementos) / 4))]
+        print(f'Terceiro Quartil = {q3}')
+#####################################################
+
+
+#             7 - AMPLITUDE
+def amplitude(valores = ()):
+    maior = max(valores)
+    menor = min(valores)
+    amplitude = maior - menor
+
+    print(f'Amplitude = {amplitude}')
+#####################################################
+
+
+#             8 - DESVIO-PADRÃO
+def desvioPadrao (valores = ()):
+    media = 0
+    tamanho = len(valores) - 1
+    indice = 0
+    valor = 0
+    soma = 0
+    variancias = [None for _ in range(tamanho + 1)]
+    varianciasQuadrada = [None for _ in range(tamanho + 1)]
     
-    # Menu:
-    print("\n-------- MENU --------")
-    print(" 1 - Entrada \n 2 - Sair")
-    print("-------- MENU --------")
+    for item in valores:
+        media += item
 
-    # Escolha do usuario:
-    menu = input("\nO que deseja fazer?\n")
+    media = media / (tamanho + 1)
 
-    # Se o usuario decidir sair:
-    if (menu == "Sair" or menu == "2"):
-        break
+    for item in valores:
+        valor = item - media
+        variancias[indice] = valor
+        indice += 1
 
-    # Entrada de valores:
-    Dados["Atuais"][0] = input("\n\nDigite um numero para a amostra: ")
-    Dados["Atuais"][1] = input("\nDigite o peso do valor anterior: ")
+    indice = 0
+
+    for item in variancias:
+        valor =  item * item
+        varianciasQuadrada[indice] = valor
+        indice += 1
+
+    soma = sum(varianciasQuadrada)
+    soma = soma / tamanho  
+    soma = math.sqrt(soma)
+
+    print(f'Desvio Padrão = {soma}\n')
+#####################################################
 
 
-    # Tentar converter:
-    try:
-        Dados["Atuais"][0] = float(Dados["Atuais"][0])
-        Dados["Atuais"][1] = float(Dados["Atuais"][1])
+#             9 - TUDO
+def tudo (valores = ()):
+    mediasimples(valores)
+    mediaponderada(valores)
+    moda(valores)
+    mediana(valores)
+    primeiroquartil(valores)
+    terceiroquartil(valores)
+    amplitude(valores)
+    desvioPadrao(valores)
 
-        Dados["Numeros"].append(Dados["Atuais"][0])
-        Dados["Pesos"].append(Dados["Atuais"][1])
+
+
+# #################INÍCIO DO PROGRAMA################
+qtde_elementos = int(input('Digite a quantidade de elementos da amostra: '))
+elementos = []
+for elemento in range(0, qtde_elementos):
+    valor = int(input(f'Digite o {elemento+1}º valor: '))
+    elementos.append(valor)
+
+# ==================MENU DE ESCOLHA==================
+print('''
+                 ---MENU---
+    | 1 - Média Aritmética Simples
+    | 2 - Média Aritmética Ponderada
+    | 3 - Moda
+    | 4 - Mediana
+    | 5 - Primeiro Quartil
+    | 6 - Terceiro Quartil
+    | 7 - Amplitude
+    | 8 - Desvio-Padrão
+    | 9 - Calcular Tudo
+    | 0 - Encerrar
     
-    # Se nao conseguir mostrar que o valor é invalido:
-    except:
-        print("O valor digitado anteriormente não é um numero valido. \n\n ")
+    ''')
+menu = int(input('Digite o número da Opção desejada: '))
 
-print (Dados["Numeros"])
-print (Dados["Pesos"])
+# =============EXECUÇÃO DOS PROCEDIMENTOS============
+if menu == 1:
+    mediasimples(elementos)
+elif menu == 2:
+    mediaponderada(elementos)
+elif menu == 3:
+    moda(elementos)
+elif menu == 4:
+    mediana(elementos)
+elif menu == 5:
+    primeiroquartil(elementos)
+elif menu == 6:
+    terceiroquartil(elementos)
+elif menu == 7:
+    amplitude(elementos)
+elif menu == 8:
+    desvioPadrao(elementos)
+elif menu == 9:
+    tudo(elementos)
